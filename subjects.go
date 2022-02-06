4b15fd9e0c144cdad3c1456490e6ccb6a389f357
Ali Shanaakh <github@shal.dev>
2022-02-06 14:30:47 +0200

    Add missing topic for customer actions in vin-decoder

--- subjects.go
+++ subjects.go
@@ -5,4 +5,5 @@ type Subject string
 const (
 	CustomerRegistrationActions = "customer.registration.actions"
 	OperationCustomerActions    = "customer.operation.actions"
+	VinDecodingCustomerActions  = "customer.vin-decoding.actions"
 )
